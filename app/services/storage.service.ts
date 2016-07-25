cea532c1c82301b25cad8c7239567aa7ab3d0be2
Doug Estey <get@dougestey.com>
2016-07-25 22:15:51 +0000

    new: storage layer

---
+++ app/services/storage.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class StorageService {
+
+  get(key) {
+    return JSON.parse(localStorage.getItem(key))
+  }
+
+  set(key, data) {
+    return localStorage.setItem(key, JSON.stringify(data))
+  }
+
+}
